48985cf6b9f9a535316f2a19ce1419e2beb22365
tanaka0625 <tanasou6@gmail.com>
2024-02-11 14:50:08 +0900

    各モデルにリレーションを貼る

---
+++ app/Models/Choice.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Choice extends Model
+{
+    use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function answers()
+    {
+        return $this->hasMany(Answer::class);
+    }
+
+}
